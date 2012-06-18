5443828fedcbe0af130821f05da5abe5330b2455
Ibrahim Jumkhawala <ibrahim_jumkhawala@mckinsey.com>
2012-06-18 16:12:40 -0400

    Comments added for core.js

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -2,43 +2,55 @@ var generatedNumbersOnPage = [];
 
 if (!window.console) console = {log: function() {}};
 
+
+//Generate a random image id from a given set of images, which between 1 and 19 right now.
 function getRandomImageId(startNum, endNum) {
   return Math.floor(startNum+(Math.random()*(endNum-startNum)));
 }
 
+//Function to fetch the random image path depending on the randomImadeId generated, uses the above function.
 function getRandomImagePath(startNum, endNum) {
   var number = getRandomImageId(startNum, endNum);
-  
+
+  //If function is called by the firstImage then the first random image is pushed.
   if(generatedNumbersOnPage.length == 0) {
     generatedNumbersOnPage.push(number);
   }
+
+  //If there already exists one number in the generatedNumbersOnPage then generate another number till they both are different.
   else if(generatedNumbersOnPage.length == 1) {
     while(number == generatedNumbersOnPage[0]) {
       number = getRandomImageId(startNum, endNum);    
     }
     generatedNumbersOnPage.push(number);
   }
+
+  //If there are two image numbers then initialize the array to have no variables and allocate the first index.
   else if(generatedNumbersOnPage.length == 2) {
     generatedNumbersOnPage = [];
     generatedNumbersOnPage.push(number);  
   }
-  
+
   return getImagePath(number);
 }
 
+//returning the image numbers on page(array)
 function getImageNumbersOnPage() {
   return generatedNumbersOnPage;
 }
 
+//Add the path of actual image to filename to get the whole path
 function getImagePath(imageNum) {
   return "/images/"+imageNum+".jpg";  
 }
+
 
 function allGeneratedImageNumbers() {
   console.log(generatedNumbers);
   $.unique(generatedNumbers);
 }
 
+//Sorts the associated array and returns an array of sorted keys in desecnding order.
 function getSortedKeysOfObject(obj) {
   return allKeys(obj).sort(function (a, b) {
     if (isNaN(a) && isNaN(b)) {
@@ -53,6 +65,7 @@ function getSortedKeysOfObject(obj) {
   });
 }
 
+//Take an object and add the keys into an array
 function allKeys(object) {
   var key, 
       keys = [];
@@ -65,6 +78,8 @@ function allKeys(object) {
   return keys;
 }
 
+
+//compares the two given arrays and gives the difference, used to get the images that have not been used.
 function diffArrays(firstArr, secondArr) {
   console.log(firstArr);
   console.log(secondArr);
